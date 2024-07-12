09d841c6f5631930935e5a2c80d3f10be64fd1dd
Ankur Srivastava <ankur.ds.aws@gmail.com>
2024-07-12 10:34:36 +0530

    new main commit231

--- evn/dev/provider.tf
+++ evn/dev/provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "3.100.0"
     }
   }
-  #   backend "azurerm" {
-  #     resource_group_name  = "tfstatestorgae"       # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-  #     storage_account_name = "tfstatestoragenetweb" # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-  #     container_name       = "terraform-tfstate"    # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-  #     key                  = "terraform.tfstate"    # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  #   }
+    backend "azurerm" {
+      resource_group_name  = "rg-tfstate"       # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+      storage_account_name = "storagetfstatecloud" # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+      container_name       = "tfstate"    # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+      key                  = "module.terraform.tfstate"    # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    }
 }
 
 provider "azurerm" {
